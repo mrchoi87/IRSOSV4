a214d75475be26089fd8436665d4431d1e9168a6
mrchoi8708@gmail.com <mrchoi8708@gmail.com>
2021-01-26 11:48:08 +0900

    test

---
+++ gate/util.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 JiNong, Inc.
+# All right reserved.
+#
+
+"""
+    Utility Functions를 정의함.
+"""
+
+import time
+import logging
+import logging.handlers
+
+def getdefaultlogger():
+    _logger = logging.getLogger('mate')
+    _logger.setLevel(logging.DEBUG)
+
+    streamHandler = logging.StreamHandler()
+    #mrchoi87
+    fileHandler = logging.FileHandler('./log')
+    #fileHandler = TimedRotatingFileHandler('./log', when='midnight', interval=1, encoding='utf-8')
+
+    formatter = logging.Formatter('[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
+
+    streamHandler.setFormatter(formatter)
+    fileHandler.setFormatter(formatter)
+
+    _logger.addHandler(streamHandler)
+    _logger.addHandler(fileHandler)
+
+    return _logger
